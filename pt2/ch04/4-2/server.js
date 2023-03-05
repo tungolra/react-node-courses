967a6fec318b6decb8683a25d2c99f2ab4079ae4
rtungol <rarrtungol@gmail.com>
2023-03-05 18:57:52 -0500

    ch4 exercises complete

--- pt2/ch04/4-2/server.js
+++ pt2/ch04/4-2/server.js
@@ -1,12 +1,23 @@
-import {createServer} from 'http';
-import {Server} from 'socket.io';
+// socket.io can raise events by name; 
+// ws spec only handles message events; all types of data have to be served to the same event 
+
+
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const server = createServer().listen(3000);
 const io = new Server(server);
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+io.on("connection", (socket) => {
+  console.log(`${io.engine.clientsCount} connections`);
+  socket.on("chat", (message) => {
+    console.log(`${socket.d}: ${message}`);
+    io.sockets.emit("message", message, socket.id);
   });
-} );
+  socket.on("disconnect", () => {
+    console.log(`${socket.id} disconnected`);
+  });
+});
+
+console.log("socket server")
+
