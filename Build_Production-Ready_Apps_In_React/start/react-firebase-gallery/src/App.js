ba7e6fed00f99d39f60631016b6a99a2fee78a37
rtungol <rarrtungol@gmail.com>
2023-04-19 20:49:21 -0400

    set up home page layout

--- Build_Production-Ready_Apps_In_React/start/react-firebase-gallery/src/App.js
+++ Build_Production-Ready_Apps_In_React/start/react-firebase-gallery/src/App.js
@@ -3,14 +3,106 @@ import "./App.css";
 
 function App() {
   return (
-    <div class="container text-center mt-5">
-      <h1>Gallery</h1>
-      <div class="row">
-        <div class="col">Column</div>
-        <div class="col">Column</div>
-        <div class="col">Column</div>
+    <>
+      {/* NavBar */}
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Link
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button class="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+      {/* Home Page */}
+      <div class="container text-center mt-5">
+        <h1>Gallery</h1>
+        <div class="row">
+          {/* Cards */}
+          {Array.apply(null, Array(10)).map((x, i) => (
+            <div class="col mb-5">
+              <div class="card" style={{ width: "18rem" }}>
+                <img
+                  src="https://via.placeholder.com/200"
+                  class="card-img-top"
+                  alt="gallery-image"
+                />
+              </div>
+            </div>
+          ))}
+          <div class="col">Column</div>
+          <div class="col">Column</div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
