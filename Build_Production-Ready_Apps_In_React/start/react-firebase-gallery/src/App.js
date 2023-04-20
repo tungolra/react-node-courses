4ca26e6140041e0c4d571c9087fa86eeef06b0be
rtungol <rarrtungol@gmail.com>
2023-04-19 20:42:01 -0400

    create-react-app, include bootstrap

---
+++ Build_Production-Ready_Apps_In_React/start/react-firebase-gallery/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div class="container text-center mt-5">
+      <h1>Gallery</h1>
+      <div class="row">
+        <div class="col">Column</div>
+        <div class="col">Column</div>
+        <div class="col">Column</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
