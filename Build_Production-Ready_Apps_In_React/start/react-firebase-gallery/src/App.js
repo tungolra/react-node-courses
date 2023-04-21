3c2f349d1954f750f3ac403fae72c9d86d683c06
rtungol <rarrtungol@gmail.com>
2023-04-21 15:16:11 -0400

    refactor business logic in app into context.js with useContext hook

--- Build_Production-Ready_Apps_In_React/start/react-firebase-gallery/src/App.js
+++ Build_Production-Ready_Apps_In_React/start/react-firebase-gallery/src/App.js
@@ -7,54 +7,56 @@ import UploadForm from "./components/UploadForm";
 
 import { photos } from "./data";
 import Layout from "./components/Layout";
+import { Context } from "./context";
 
-const initialState = {
-  items: photos,
-  count: photos.length,
-  inputs: { title: null, file: null, path: null },
-  isCollapsed: false,
-};
+// const initialState = {
+//   items: photos,
+//   count: photos.length,
+//   inputs: { title: null, file: null, path: null },
+//   isCollapsed: false,
+// };
 
-const handleChange = (state, e) => {
-  if (e.target.name === "file") {
-    return {
-      ...state.inputs,
-      file: e.target.files[0],
-      path: URL.createObjectURL(e.target.files[0]),
-    };
-  } else {
-    return { ...state.inputs, title: e.target.value };
-  }
-};
+// const handleChange = (state, e) => {
+//   if (e.target.name === "file") {
+//     return {
+//       ...state.inputs,
+//       file: e.target.files[0],
+//       path: URL.createObjectURL(e.target.files[0]),
+//     };
+//   } else {
+//     return { ...state.inputs, title: e.target.value };
+//   }
+// };
 
-function reducer(state, action) {
-  switch (action.type) {
-    case "setItem":
-      return {
-        ...state,
-        count: state.items.length + 1,
-        items: [state.inputs, ...state.items],
-        inputs: { title: null, file: null, path: null },
-      };
-    case "setInputs":
-      return {
-        ...state,
-        inputs: handleChange(state, action.payload.value),
-      };
-    case "collapse":
-      return {
-        ...state,
-        isCollapsed: action.payload.bool,
-      };
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case "setItem":
+//       return {
+//         ...state,
+//         count: state.items.length + 1,
+//         items: [state.inputs, ...state.items],
+//         inputs: { title: null, file: null, path: null },
+//       };
+//     case "setInputs":
+//       return {
+//         ...state,
+//         inputs: handleChange(state, action.payload.value),
+//       };
+//     case "collapse":
+//       return {
+//         ...state,
+//         isCollapsed: action.payload.bool,
+//       };
 
-    default:
-      return state;
-  }
-}
+//     default:
+//       return state;
+//   }
+// }
 
 function App() {
-  const [state, dispatch] = React.useReducer(reducer, initialState);
+  // const [state, dispatch] = React.useReducer(reducer, initialState);
   // const [count, setCount] = React.useState("");
+  const {state, dispatch} = React.useContext(Context)
 
   const toggle = (bool) => dispatch({ type: "collapse", payload: { bool } });
 
