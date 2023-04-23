4ce6c642d0796cdcfdf98d8c46300cc01924e383
rtungol <rarrtungol@gmail.com>
2023-04-23 19:09:29 -0400

    configure auth sdk

---
+++ Build_Production-Ready_Apps_In_React/start/react-firebase-gallery/src/utils/auth.js
@@ -0,0 +1,27 @@
+import {
+  getAuth,
+  signOut,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { auth } from "../lib/firebase.config";
+
+const provider = new GoogleAuthProvider();
+
+const FirebaseAuth = {
+  signIn: () => {
+    return new Promise((resolve) => {
+      signInWithPopup(auth, provider)
+        .then((response) => {
+          resolve(response.user);
+        })
+        .catch(console.error);
+    });
+  },
+
+  signOut: () => {
+    signOut(auth)
+      .then(() => console.log("User signed out!"))
+      .catch(console.error);
+  },
+};
