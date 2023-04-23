8f59f8a7215b7b5c4aa91733345d49b211cc6000
rtungol <rarrtungol@gmail.com>
2023-04-23 18:50:43 -0400

    format cards with date, title, username

--- Build_Production-Ready_Apps_In_React/start/react-firebase-gallery/src/components/Card.js
+++ Build_Production-Ready_Apps_In_React/start/react-firebase-gallery/src/components/Card.js
@@ -1,14 +1,27 @@
+import { P } from "morse/map";
 import React from "react";
 
-export default function Card({ path, title }) {
+export default function Card({ path, title, createdAt }) {
+  const timestamp = React.useMemo(() => {
+    const date = new Date(createdAt.seconds * 1000);
+    return date.toLocaleDateString();
+  }, []);
   return (
     <div className="col mb-5">
       <div className="card" style={{ width: "18rem" }}>
-        <img
-          src={path}
-          className="card-img-top"
-          alt={title}
-        />
+        <div
+          style={{
+            height: "220px",
+            backgroundImage: `url(${path})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+        ></div>
+        <h5 className="text-center">{title}</h5>
+        <div className="d-flex justify-content-between px-2">
+          <p className="text-center ">{timestamp}</p>
+          <i>@username</i>
+        </div>
       </div>
     </div>
   );
